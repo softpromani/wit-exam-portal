f1f14dd343bc90484e8728fc26c84284c09a4190
unknown <shivam786098jais00@gmail.com>
2024-08-13 05:11:34 +0530

    exam-form error fix

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Http\Controllers\Admin\AdminController;
-use App\Http\Controllers\student\ExamFormController;
+use App\Http\Controllers\Student\ExamFormController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\Admin\Auth\AdminAuthController;
