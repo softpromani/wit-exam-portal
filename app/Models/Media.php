d8ed16a229321bccc97e614d418ac19af2d74584
unknown <shivam786098jais00@gmail.com>
2024-08-13 19:57:51 +0530

    medaifile

--- app/Models/Media.php
+++ app/Models/Media.php
@@ -2,9 +2,10 @@
 
 namespace App\Models;
 
-se Illuminate\Database\Eloquent\SoftDeletes;use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-u
+
 use Illuminate\Http\UploadedFile;
 class Media extends Model
 {
