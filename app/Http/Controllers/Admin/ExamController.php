df77b3ba5d22bdd7bede75fbe663f62b297de55b
unknown <softpromani@gmail.com>
2024-08-17 12:20:29 +0530

    exam form

--- app/Http/Controllers/Admin/ExamController.php
+++ app/Http/Controllers/Admin/ExamController.php
@@ -12,7 +12,7 @@ class ExamController extends Controller
 
 
     public function exam_form_list(){
-        $examformdata=ExamForm::get();
+        $examformdata=ExamForm::with('student')->get();
         // dd($examformdata);
         return view('admin.exam.exam-form-list',compact('examformdata'));
     }
