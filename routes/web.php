2954b089fe6274826436d46ee62aaf6bad2a79bc
unknown <softpromani@gmail.com>
2024-08-20 22:53:52 +0530

    subject check

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\Student\StudentController;
 use App\Http\Controllers\Admin\ExamController;
 use App\Http\Controllers\Admin\PaymentController;
 use App\Http\Controllers\FeePaymentController;
+use App\Http\Controllers\Student\AdmitCardController;
 
 /*
 |--------------------------------------------------------------------------
@@ -29,6 +30,7 @@ Route::group(['prefix' => 'student', 'as' => 'student.'], function () {
     Route::post('student-profile-store',[StudentController::class,'store'])->name('store');
     Route::post('change-password/{id}',[AuthController::class,'changePassword'])->name('changePassword');
     Route::get('logout', [StudentController::class, 'logout'])->name('logout');
+    Route::get('admitcard', [AdmitCardController::class, 'studentAdmitcard'])->name('admit-card');
 
 
     Route::group(['prefix'=>'semester','as'=>'semester.'],function(){
@@ -36,6 +38,9 @@ Route::group(['prefix' => 'student', 'as' => 'student.'], function () {
         Route::post('subject-fetch',[ExamFormController::class,'subject_fetch'])->name('fetchsubject');
         Route::get('exam-for-form/{id}',[ExamFormController::class,'apply_for_exam'])->name('exam-for-apply');
         Route::post('exam-apply',[ExamFormController::class,'apply_exam_form'])->name('exam-apply');
+        Route::get('admitcard-form-list',[ExamFormController::class,'admitcard_form_list'])->name('admitcard-form-list');
+        Route::get('admitcard-download/{exam_session_id}',[ExamFormController::class,'admitcard_download'])->name('admitcard-download');
+        Route::get('locked-subject-by-examsession/{exam_Session_id}',[ExamFormController::class, 'locked_subject_by_examsession'])->name('locked-subject-by-examsession');
 
     });
 });
@@ -46,7 +51,11 @@ Route::group(['prefix' => 'admin', 'as' => 'admin.'], function () {
     Route::get('admin-dashboard',[AdminController::class,'adminDashboard'])->name('admin-dashboard');
     Route::get('logout', [AdminAuthController::class, 'adminLogout'])->name('admin-logout');
     Route::get('exam-form-list',[ExamController::class,'exam_form_list'])->name('exam-form_list');
+    Route::get('exam-schedule-list',[ExamController::class,'exam_schedule'])->name('exam_schedule_list');
+    Route::post('exam-schedule',[ExamController::class,'exam_schedule_store'])->name('exam_schedule_store');
+    Route::get('subject',[ExamController::class,'subject'])->name('exam_subjects');
     Route::post('fee-payment',[PaymentController::class,'feePayment'])->name('fee-payment');
-    
+
+
 
 });
