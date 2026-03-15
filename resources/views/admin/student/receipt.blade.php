be5a10466f9f2115c7cf9bc22a503bd26af3b60b
ShahzadeFortaxe <softpromani@gmail.com>
2026-03-15 22:25:12 +0530

    add text on slip

--- resources/views/admin/student/receipt.blade.php
+++ resources/views/admin/student/receipt.blade.php
@@ -82,6 +82,7 @@
                     <div class="col-10 text-center">
                         <div class="header-title">LALIT NARAYAN MITHILA UNIVERSITY</div>
                         <div class="header-sub">Kameshwarnagar, Darbhanga</div>
+                        <div class="header-sub">REGISTRATION SLIP</div>
                     </div>
                 </div>
 
@@ -90,9 +91,10 @@
                         <b>Registration no : {{ $student->registration_no ?? 'N/A' }}</b>
                     </div>
                     <div class="col-3">
-                        <b>Registration Year : {{ \Carbon\Carbon::parse($student->admission_session->from)->year ?? 'N/A' }}</b>
+                        <b>Registration Year :
+                            {{ \Carbon\Carbon::parse($student->admission_session->from)->year ?? 'N/A' }}</b>
                     </div>
-                     <div class="col-3">
+                    <div class="col-3">
                         <b>Session : {{ $student->admission_session->session_name ?? 'N/A' }}</b>
                     </div>
                     <div class="col-3">
@@ -102,7 +104,7 @@
 
                 <div class="row border-bottom py-1">
                     <div class="col-3">
-                        <b>College Code & Name</b><br /><br/>
+                        <b>College Code & Name</b><br /><br />
                         <b>Name of Student</b><br />
                         <b>Father's Name</b><br />
                         <b>University Roll No.</b><br />
@@ -127,7 +129,8 @@
 
                 <div class="row border-bottom py-2" style="min-height: 50px;">
                     <div class="col-9 align-self-center">
-                        <b>Subject : </b> {{ $student->course->full_name ?? $student->course->name ?? 'N/A' }} in {{ $student->branch->full_name ?? $student->branch->name ?? 'N/A' }}
+                        <b>Subject : </b> {{ $student->course->full_name ?? $student->course->name ?? 'N/A' }} in
+                        {{ $student->branch->full_name ?? $student->branch->name ?? 'N/A' }}
                     </div>
                     <div class="col-3 sign text-center border-start align-self-center">
                         @if($student->sign)
@@ -146,9 +149,11 @@
 
                 <div class="row border-top pt-1" style="font-size: 10px;">
                     <div class="col-12">
-                        <b>Important Directions: </b> Please Keep this slip safely for future reference.<br/> Please ensure all
+                        <b>Important Directions: </b> Please Keep this slip safely for future reference.<br /> Please ensure
+                        all
                         the
-                        information printed are correct.<br/> Please Contact your college within fifteen days for any correction.
+                        information printed are correct.<br /> Please Contact your college within fifteen days for any
+                        correction.
                     </div>
                 </div>
             </div>
